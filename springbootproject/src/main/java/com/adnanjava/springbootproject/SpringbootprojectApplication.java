f93885e0c7ed75f22de6276986d0f7982b8dda81
mohammed adnan <mohammedadnan190201@gmail.com>
2025-09-01 15:25:23 +0300

    Initial Spring Boot project commit

---
+++ springbootproject/src/main/java/com/adnanjava/springbootproject/SpringbootprojectApplication.java
@@ -0,0 +1,14 @@
+package com.adnanjava.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectApplication.class, args);
+	}
+
+}
---
+++ springbootproject/src/test/java/com/adnanjava/springbootproject/SpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adnanjava.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
